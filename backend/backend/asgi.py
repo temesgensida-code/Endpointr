92d51955c5d231180ae7d19eda77600a438afa87
Temesgen Sida <temesgensids@gmail.com>
2026-07-28 12:09:57 +0300

    fix(backend): initialize DJANGO_SETTINGS_MODULE before importing app routing in asgi.py

--- backend/backend/asgi.py
+++ backend/backend/asgi.py
@@ -9,15 +9,15 @@ https://docs.djangoproject.com/en/6.0/howto/deployment/asgi/
 
 import os
 
-from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
-
-import AI_handler.routing
-import realtime.routing
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.settings')
 
 django_asgi_app = get_asgi_application()
+
+from channels.routing import ProtocolTypeRouter, URLRouter
+import AI_handler.routing
+import realtime.routing
 
 ws_patterns = AI_handler.routing.websocket_urlpatterns + realtime.routing.websocket_urlpatterns
 
