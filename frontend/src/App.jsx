6e279ee08545f2f29a41ad327fabe573c7a35de4
Temesgen Sida <temesgensids@gmail.com>
2026-07-31 11:21:22 +0300

    refactor(app): remove Clerk useAuth and render AppShell directly

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,18 +1,7 @@
-import { useAuth } from '@clerk/react'
-import SignedOutPanel from './components/auth/SignedOutPanel'
 import AppShell from './components/layout/AppShell'
 import './index.css'
 
 export default function App() {
-  const { isSignedIn, isLoaded } = useAuth()
-
-  if (!isLoaded) {
-    return (
-      <div style={{ height: '100vh', display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
-        <div className="spinner" style={{ width: 28, height: 28 }} />
-      </div>
-    )
-  }
-
-  return isSignedIn ? <AppShell /> : <SignedOutPanel />
+  return <AppShell />
 }
+
